a861e1cd9d8b5a78064c00074fef0f02e27af095
kamikaonashi <thorschi93@gmx.at>
2024-06-10 20:25:50 +0200

    venus: remove yupik & shima leftovers

--- rootdir/bin/init.kernel.post_boot.sh
+++ rootdir/bin/init.kernel.post_boot.sh
@@ -38,13 +38,6 @@ case "$platformid" in
     "415"|"439"|"456"|"501"|"502")
 	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-lahaina.sh
 	;;
-
-    "450")
-	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-shima.sh
-	;;
-    "475"|"499"|"487"|"488"|"498"|"497"|"515")
-	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-yupik.sh
-	;;
      *)
 	echo "***WARNING***: Invalid SoC ID\n\t No postboot settings applied!!\n"
 	;;
