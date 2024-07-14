9a85e562700ca21577739c5ef3fb1c4bae1258a6
kamikaonashi <thorschi93@gmx.at>
2024-07-14 14:25:07 +0200

    venus: add Viper4FX support
    according to https://github.com/TogoFire/packages_apps_ViPER4AndroidFX
    
    add "if-exists" to call

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Call the ViperFX Config
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
