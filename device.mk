3b8aa6eeca4007411f9c0b6e8d190a3d723b5fb4
Wei Wang <wvw@google.com>
2024-08-03 15:19:40 +0200

    venus: remove QTI perfd

--- device.mk
+++ device.mk
@@ -392,9 +392,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # PowerShare
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.xiaomi
